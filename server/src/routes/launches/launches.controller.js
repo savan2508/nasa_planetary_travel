b0e1f9a3c3216aacdd31b4507bfe974e315997bb
Savan <sawanpatel2508@gmail.com>
2023-11-23 22:36:17 -0500

    Add httpAddNewLaunch function in launches.controller.js.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,22 @@
-const { getAllLaunches } = require("../../models/launches.models");
+const {
+  getAllLaunches,
+  addNewLaunch,
+} = require("../../models/launches.models");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  launch.launchDate = new Date(launch.launchDate);
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
 module.exports = {
   httpGetAllLaunches,
+  httpAddNewLaunch,
 };
