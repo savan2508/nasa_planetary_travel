ffdb727dea96f04f228b412574cca3068b6115d5
savan2508 <sawanpatel2508@gmail.com>
2024-01-01 15:54:00 -0500

    Fix the mongodb and mongoose callback

--- client/src/pages/Launch.js
+++ client/src/pages/Launch.js
@@ -4,11 +4,13 @@ import Clickable from "../components/Clickable";
 
 const Launch = (props) => {
   const selectorBody = useMemo(() => {
-    return props.planets?.map((planet) => (
-      <option value={planet.kepler_name} key={planet.kepler_name}>
-        {planet.kepler_name}
-      </option>
-    ));
+    return Array.isArray(props.planets)
+      ? props.planets.map((planet) => (
+          <option value={planet.keplerName} key={planet.keplerName}>
+            {planet.keplerName}
+          </option>
+        ))
+      : null;
   }, [props.planets]);
 
   const today = new Date().toISOString().split("T")[0];
--- server/src/models/planets.models.js
+++ server/src/models/planets.models.js
@@ -2,8 +2,7 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
 
-const results = [];
-const habitablePlanet = [];
+const Planets = require("./planets.mongo");
 
 const isHabitablePlanet = (planet) => {
   return (
@@ -25,27 +24,50 @@ function loadPlanetsDate() {
           columns: true,
         }),
       )
-      .on("data", (data) => {
-        results.push(data);
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanet.push(data);
+          savePlanet(data);
         }
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
         console.log(
-          `${habitablePlanet.length} habitable planets found from the total ${results.length} planets discovered by Kepler!`,
+          `${countPlanetsFound} habitable planets found from the total planets discovered by Kepler!`,
         );
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanet;
+async function getAllPlanets() {
+  try {
+    return await Planets.find();
+  } catch (error) {
+    console.error(`Error fetching planets: ${error}`);
+    throw error; // Propagate the error to the caller if needed
+  }
+}
+
+async function savePlanet(planet) {
+  try {
+    await Planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      },
+    );
+  } catch (err) {
+    console.error(`Could not save planet ${err}`);
+  }
 }
 
 module.exports = {
